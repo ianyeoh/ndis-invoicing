e5814a6929055ed5c3ed36a2ea0e56258c01b507
Ian Yeoh <ian.yeoh.st@prysmian.com>
2025-05-01 14:56:06 +1000

    Remove duplicated folder from merge

--- frontend/src/app/(protected)/app/codes/page.tsx
+++
@@ -1,5 +0,0 @@
-import NDISCodeVisualiser from "@/components/ndis-codes/ndis-code-visualiser";
-
-export default function NDISCodeConfigurator() {
-    return <NDISCodeVisualiser />;
-}
--- frontend/src/lib/google-oauth2.ts
+++
@@ -1,6 +0,0 @@
-import { Session } from "next-auth";
-
-export function isScopeGranted(session: Session, scope: string) {
-    const scopeList = session.scope.split(" ");
-    return scopeList.includes(scope);
-}
